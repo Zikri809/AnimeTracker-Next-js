ce9219b4090d8ac3df7f24ff11da88a1a776d69d
Zikri <161694337+Zikri809@users.noreply.github.com>
2026-03-07 06:57:55 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,7 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
    images: {
-    remotePatterns: [new URL('https://cdn.myanimelist.net/**'), new URL('https://s4.anilist.co/**')],
+    remotePatterns: [new URL('https://myanimelist.cdn-dena.com/**'), new URL('https://s4.anilist.co/**')],
   },
   
 };
